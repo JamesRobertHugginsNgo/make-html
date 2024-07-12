d183eaf549d4e1b5a18c93ee9ee5dbf2cfeafa94
James Robert Huggins Ngo <jamesroberthugginsngo@gmail.com>
2024-07-12 13:12:17 -0400

    Remove js doc

--- add-classes.js
+++ add-classes.js
@@ -1,9 +1,3 @@
-/**
- * Add a list of class names to an HTML element.
- * @param {HTMLElement} element The HTML element.
- * @param {[any]} classes A list of class names. Undefined or null values are ignored.
- * @returns {HTMLElement}
- */
 export default function addClasses(element, classes) {
 	const value = classes.filter((className) => {
 		return className != null;
--- set-styles.js
+++ set-styles.js
@@ -1,9 +1,3 @@
-/**
- * Sets a list of style properties to an HTML element.
- * @param {HTMLElement} element The HTML element.
- * @param {object} styles The list of style properties. Undefined or null values are ignored.
- * @returns {HTMLElement}
- */
 export default function setStyles(element, styles) {
 	for (const property in styles) {
 		const value = styles[property];
